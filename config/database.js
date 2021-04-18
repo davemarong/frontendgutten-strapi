19057e21013f5630cb0ff1f107336d04b7a66d88
Dave Marong <kmarong@hotmail.com>
2021-04-18 09:49:22 +0200

    gfhgh

--- config/database.js
+++ config/database.js
@@ -18,6 +18,7 @@ module.exports = ({ env }) => {
           },
           options: {
             ssl: {
+              require: true,
               rejectUnauthorized: false,
             },
           },
